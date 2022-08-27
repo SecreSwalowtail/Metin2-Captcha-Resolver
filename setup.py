302fcf12b7482f401550c0c8596c11513c303439
Secre <robytza12335@gmail.com>
2022-08-27 15:14:15 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='main.py',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Open Software License 3.0',
+    author='SecreSwalowtail#4779',
+    author_email='',
+    description='Nostalgia 2 Captcha Resolver'
+)
